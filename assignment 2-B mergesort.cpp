ee99ba7536f56867bc7996894ccfdcea17fd7f6a
shish5630 <117890266+shish5630@users.noreply.github.com>
2023-05-21 18:04:50 +0530

    Update assignment 2-B mergesort.cpp

--- assignment 2-B mergesort.cpp
+++ assignment 2-B mergesort.cpp
@@ -1,56 +1,58 @@
-#include <bits/stdc++.h>
-#include <vector>
-#include <omp.h>
-
+#include<bits/stdc++.h>
+#include<omp.h>
 using namespace std;
 
-void merge(vector<int>& arr, int left, int middle, int right) {
-    vector<int> L(arr.begin() + left, arr.begin() + middle + 1);
-    vector<int> R(arr.begin() + middle + 1, arr.begin() + right + 1);
-
-    int i = 0, j = 0, k = left;
-    while (i < L.size() && j < R.size()) {
-        if (L[i] <= R[j])
+void merge(vector<int>&arr,int left, int middle,int right){
+    vector<int>L(arr.begin()+left,arr.begin()+middle+1);
+    vector<int>R(arr.begin()+middle+1,arr.begin()+right+1);
+    int i=0;int j=0;int k = left;
+    while(i<L.size()&& j<R.size()){
+        if(L[i]<=R[j]){
             arr[k++] = L[i++];
-        else
+        }else{
             arr[k++] = R[j++];
+        }
     }
-
-    while (i < L.size())
+    while(i<L.size()){
         arr[k++] = L[i++];
-
-    while (j < R.size())
+    }
+     while(j<R.size()){
         arr[k++] = R[j++];
+    }
+    
 }
-
-void mergeSort(vector<int>& arr, int left, int right) {
-    if (left >= right)
+void mergeSort(vector<int>&arr,int left ,int right){
+    if(left>=right){
         return;
-
-    int middle = left + (right - left) / 2;
-
+    }
+    int middle = left+ (right-left )/2;
     #pragma omp parallel sections
     {
-        #pragma omp section
-        mergeSort(arr, left, middle);
-
-        #pragma omp section
-        mergeSort(arr, middle + 1, right);
+        #pragma omp parallel section 
+        
+        {
+            mergeSort(arr,left,middle);
+            
+        }
+        #pragma omp parallel section 
+        
+        {
+            mergeSort(arr,middle+1,right);
+            
+        }
     }
-
-    merge(arr, left, middle, right);
+    merge(arr, left , middle, right);
+    
 }
 
-int main() {
-    vector<int> arr = {12, 11, 13, 5, 6, 7};
-    int n = arr.size();
-
-    mergeSort(arr, 0, n - 1);
-
-    cout << "Sorted array: ";
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
-    cout << endl;
-
+int main(){
+    vector<int>arr={10,12,23,21,4,3,2};
+    int n  =  arr.size();
+    mergeSort(arr,0,n-1);
+    cout<<"sorted array is below \n";
+    for(int i =0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
     return 0;
 }
+
